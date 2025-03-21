5c4493ebd6500721e252e98fa714ce95e96bd40c
MuuShuu <MuuShuu@users.noreply.github.com>
2025-03-21 13:37:21 +0200

    renaming

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,29 +7,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const currentState = { ...state };
+  const stateCopy = { ...state };
   const stateHistory = [];
 
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
-        Object.assign(currentState, action.extraData);
+        Object.assign(stateCopy, action.extraData);
         break;
 
       case 'removeProperties':
         for (const key of action.keysToRemove) {
-          delete currentState[key];
+          delete stateCopy[key];
         }
         break;
 
       case 'clear':
-        for (const key in currentState) {
-          delete currentState[key];
+        for (const key in stateCopy) {
+          delete stateCopy[key];
         }
         break;
     }
 
-    stateHistory.push({ ...currentState });
+    stateHistory.push({ ...stateCopy });
   }
 
   return stateHistory;
